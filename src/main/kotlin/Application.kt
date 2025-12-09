fb3c207747d51fa29378867695786108d38cb904
RegisCT03 <maregina861@gmail.com>
2025-12-09 09:35:13 -0600

    Admin seeder added

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,6 +1,8 @@
 package com.example
 
+import com.example.application.dtos.request.UserRegisterRequestDto
 import com.example.application.services.JwtService
+import com.example.domain.models.User
 import com.example.infrastructure.database.DatabaseFactory
 import com.example.presentation.di.DependenciesDeclaration
 import com.example.presentation.plugins.configSecurity
@@ -9,6 +11,8 @@ import com.example.presentation.routing.configureRouting
 import io.ktor.server.application.*
 import io.ktor.http.*
 import io.ktor.server.plugins.cors.routing.*
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
@@ -31,6 +35,24 @@ fun Application.module() {
     configureSerialization()
     configSecurity(jwtService)
     configureRouting (jwtService)
+    launch {
+        var adminUser = DependenciesDeclaration.userService.getUserByEmail("admin@example.com")
+        if (adminUser == null) {
+            DependenciesDeclaration.userRepository.createUser(
+                User(
+                    roleId = 1,
+                    email = "admin@example.com",
+                    firstName = "Admin",
+                    lastName = "Admin",
+                    phone = "9611969619",
+                    password = DependenciesDeclaration.passwordService.hashPassword("12345"),
+                    createdAt = LocalDateTime.now(),
+                    updatedAt = LocalDateTime.now(),
+                    stripeId = ""
+                )
+            )
+        }
+    }
 }
 
 suspend fun seedApp(){
